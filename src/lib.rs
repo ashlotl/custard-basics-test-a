bf035b7d2ba9dd9ec6bb0dfb0c3c1d4f4ede626b
Ashlotl <thesupremeliverwurst@gmail.com>
2022-02-27 19:26:17 -0800

    Got datachunk loading working

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,18 @@
-//TODO: Plagiarize glue code from engineengine and test datachunk loading
+use custard_macros::attach_datachunk;
+use custard_use::{
+	dylib_management::safe_library::load_types::{DatachunkLoadFn, FFIResult, FFISafeString},
+	user_types::datachunk::Datachunk,
+};
+
+use serde::Deserialize;
+
+#[derive(Debug, Deserialize)]
+pub struct TestDatachunkA {
+	field_a: bool,
+	field_b: u32,
+	field_c: String,
+}
+
+impl Datachunk for TestDatachunkA {}
+
+attach_datachunk!(TestDatachunkA);
