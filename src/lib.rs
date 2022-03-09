94f55c0903e9349b23df9652f4d22a99535bee0b
Ashlotl <thesupremeliverwurst@gmail.com>
2022-03-09 01:15:14 -0800

    got tasks and full reloads working

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,17 @@
 use std::{
 	cell::RefCell,
-	sync::{Arc, RwLock},
+	sync::{Arc, Mutex},
 	time::SystemTime,
 };
 
 use custard_macros::{attach_datachunk, attach_task};
 
 use custard_use::{
-	errors::tasks_result::TasksResult,
+	identify::{custard_name::CustardName, task_name::FullTaskName},
 	user_types::{
 		datachunk::Datachunk,
-		task::{Task, TaskClosureType},
+		task::{Task, TaskClosureType, TaskData, TaskImpl},
+		task_control_flow::task_control_flow::TaskControlFlow,
 	},
 };
 
@@ -28,41 +29,65 @@ impl Datachunk for TestDatachunkA {}
 attach_datachunk!(TestDatachunkA);
 
 #[derive(Debug, Deserialize)]
-pub struct TestTaskA {
-	counter: RefCell<u32>,
+pub struct TestTaskAData {
+	// #[serde(default = "set_counter_default")]
+	counter: Mutex<u32>,
 	funny_string: String,
 	#[serde(default = "set_time_default")]
-	time: RefCell<SystemTime>,
+	time: Mutex<SystemTime>,
 }
 
-fn set_time_default() -> RefCell<SystemTime> {
+impl TaskData for TestTaskAData {}
+
+#[derive(Debug)]
+pub struct TestTaskAImpl();
+
+fn set_counter_default() -> Mutex<u32> {
+	Mutex::new(0)
+}
+
+fn set_time_default() -> Mutex<SystemTime> {
 	//we're actually going to completely ignore this initial value in order to get a more accurate result, but consider it a tutorial
-	RefCell::new(SystemTime::now())
+	Mutex::new(SystemTime::now())
 }
 
-impl Task for TestTaskA {
-	fn run(self: Arc<Self>) -> TaskClosureType {
-		Box::new(move |_value: Arc<RwLock<TasksResult>>| {
-			let mut counter = self.counter.borrow_mut();
+impl TaskImpl for TestTaskAImpl {
+	fn handle_control_flow_update(&self, task_data: &dyn TaskData, this_name: &FullTaskName, other_name: &FullTaskName, control_flow: &TaskControlFlow) -> bool {
+		true
+	}
+
+	fn run(&self, _task_data: &dyn TaskData, task_name: FullTaskName) -> TaskClosureType {
+		Box::new(Mutex::new(move |data: Arc<Mutex<dyn TaskData>>| {
+			let object = data.lock().unwrap();
+			let data = object.downcast_ref::<TestTaskAData>().unwrap();
+			let mut counter = data.counter.lock().unwrap();
+			let mut time = data.time.lock().unwrap();
+
 			if *counter == 0 {
-				let mut time = self.time.borrow_mut();
 				*time = SystemTime::now();
 			}
+
 			*counter += 1;
+
+			if *counter == 10_000 && task_name.task_name.get() == "test-task-a" {
+				let time_since_last = time.elapsed().unwrap();
+
+				println!("time elapsed: {}", time_since_last.as_nanos());
+				println!("counter: {}", counter);
+
+				return TaskControlFlow::Reload;
+			}
+
+			TaskControlFlow::Continue
 			// println!("hello from task closure: {}", self.funny_string);
-
-			Ok(())
-		})
+		}))
 	}
 }
 
-impl Drop for TestTaskA {
+impl Drop for TestTaskAData {
 	fn drop(&mut self) {
-		let time = self.time.borrow();
-		let time_since_last = time.elapsed().unwrap();
-		println!("time elapsed: {}", time_since_last.as_nanos());
-		println!("counter: {}", *self.counter.borrow());
+		println!("drop works: {}", self.funny_string)
 	}
 }
 
-attach_task!(TestTaskA);
+attach_task!["TestTaskAImpl:TestTaskAData"];
