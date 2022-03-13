93dd49e02d8ab52631d52457117a31dd0bb8d5fa
Ashlotl <thesupremeliverwurst@gmail.com>
2022-03-12 23:09:09 -0800

    got partial reloading sort-of working

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use std::{
-	cell::RefCell,
+	rc::Rc,
 	sync::{Arc, Mutex},
 	time::SystemTime,
 };
@@ -7,10 +7,15 @@ use std::{
 use custard_macros::{attach_datachunk, attach_task};
 
 use custard_use::{
-	identify::{custard_name::CustardName, task_name::FullTaskName},
+	errors::task_composition_errors::custard_not_in_cycle_error::CustardNotInCycleError,
+	identify::{
+		crate_name::CrateName,
+		custard_name::CustardName,
+		task_name::{FullTaskName, TaskName},
+	},
 	user_types::{
 		datachunk::Datachunk,
-		task::{Task, TaskClosureType, TaskData, TaskImpl},
+		task::{TaskClosureType, TaskData, TaskImpl},
 		task_control_flow::task_control_flow::TaskControlFlow,
 	},
 };
@@ -30,7 +35,6 @@ attach_datachunk!(TestDatachunkA);
 
 #[derive(Debug, Deserialize)]
 pub struct TestTaskAData {
-	// #[serde(default = "set_counter_default")]
 	counter: Mutex<u32>,
 	funny_string: String,
 	#[serde(default = "set_time_default")]
@@ -42,17 +46,14 @@ impl TaskData for TestTaskAData {}
 #[derive(Debug)]
 pub struct TestTaskAImpl();
 
-fn set_counter_default() -> Mutex<u32> {
-	Mutex::new(0)
-}
-
 fn set_time_default() -> Mutex<SystemTime> {
 	//we're actually going to completely ignore this initial value in order to get a more accurate result, but consider it a tutorial
 	Mutex::new(SystemTime::now())
 }
 
 impl TaskImpl for TestTaskAImpl {
-	fn handle_control_flow_update(&self, task_data: &dyn TaskData, this_name: &FullTaskName, other_name: &FullTaskName, control_flow: &TaskControlFlow) -> bool {
+	fn handle_control_flow_update(&self, _task_data: &dyn TaskData, _this_name: &FullTaskName, _other_name: &FullTaskName, _control_flow: &TaskControlFlow) -> bool {
+		//any kind of control flow update causes this to quit
 		true
 	}
 
@@ -69,17 +70,28 @@ impl TaskImpl for TestTaskAImpl {
 
 			*counter += 1;
 
-			if *counter == 10_000 && task_name.task_name.get() == "test-task-a" {
-				let time_since_last = time.elapsed().unwrap();
+			println!("Counter: {}", counter);
+			println!("Enter your option and press enter:\nContinue (c)\nError(e)\nFullReload(f)\nPartialReload(p)\nStopAll(a)\nStopThis(s)\npanic(!)\n");
 
-				println!("time elapsed: {}", time_since_last.as_nanos());
-				println!("counter: {}", counter);
+			let mut input_string = "".to_owned();
 
-				return TaskControlFlow::Reload;
-			}
+			std::io::stdin().read_line(&mut input_string).unwrap();
 
-			TaskControlFlow::Continue
-			// println!("hello from task closure: {}", self.funny_string);
+			input_string.retain(|c| !c.is_whitespace());
+
+			return match input_string.as_str() {
+				"c" => TaskControlFlow::Continue,
+				"e" => TaskControlFlow::Err(Rc::new(CustardNotInCycleError { offending_task: task_name.clone() })),
+				"f" => TaskControlFlow::FullReload,
+				"p" => TaskControlFlow::PartialReload,
+				"a" => TaskControlFlow::StopAll,
+				"s" => TaskControlFlow::StopThis,
+				"!" => panic!("user-triggered panic"),
+				_ => {
+					println!("Not a command!");
+					TaskControlFlow::Continue
+				}
+			};
 		}))
 	}
 }
