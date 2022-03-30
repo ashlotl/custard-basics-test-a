0710e3e02e758b937193e2f8c984331a982761ca
Ashlotl <thesupremeliverwurst@gmail.com>
2022-03-30 00:36:55 -0700

    datachunks are working

--- src/lib.rs
+++ src/lib.rs
@@ -8,11 +8,12 @@ use std::{
 use custard_macros::{attach_datachunk, attach_task};
 
 use custard_use::{
+	composition::loaded::datachunk_getter::DatachunkGetter,
 	concurrency::possibly_poisoned_mutex::PossiblyPoisonedMutex,
 	errors::task_composition_errors::custard_not_in_cycle_error::CustardNotInCycleError,
-	identify::task_name::FullTaskName,
+	identify::{datachunk_name::FullDatachunkName, task_name::FullTaskName},
 	user_types::{
-		datachunk::Datachunk,
+		datachunk::Datachunkable,
 		task::{TaskClosureType, Taskable},
 		task_control_flow::task_control_flow::TaskControlFlow,
 	},
@@ -27,7 +28,7 @@ pub struct TestDatachunkA {
 	field_c: String,
 }
 
-impl Datachunk for TestDatachunkA {}
+impl Datachunkable for TestDatachunkA {}
 
 attach_datachunk!(TestDatachunkA);
 
@@ -50,8 +51,17 @@ impl Taskable for TestTaskA {
 		false
 	}
 
-	fn run(&mut self, task_name: FullTaskName) -> TaskClosureType {
+	fn run(&mut self, task_name: FullTaskName, datachunk_getter: Arc<DatachunkGetter>) -> TaskClosureType {
+		let mut shared_data = if task_name == FullTaskName::new("custard-basics-test-a".to_owned(), "test-task-c".to_owned()) { Some(datachunk_getter.get_mut::<TestDatachunkA>(&FullDatachunkName::new("custard-basics-test-a".to_owned(), "test-datachunk-a".to_owned())).or_panic()) } else { None };
+
 		Box::new(Mutex::new(move |data: Arc<PossiblyPoisonedMutex<dyn Taskable>>| {
+			//play with datachunk
+			if let Some(v) = &mut shared_data {
+				v.field_a = !v.field_a;
+				v.field_b *= 2;
+				println!("{}, {}, {}", v.field_a, v.field_b, v.field_c);
+			}
+
 			let object = data.lock();
 			let data = object.downcast_ref::<TestTaskA>().unwrap();
 			let mut counter = data.counter.lock().unwrap();
@@ -69,7 +79,6 @@ impl Taskable for TestTaskA {
 			let mut input_string = "".to_owned();
 
 			std::io::stdin().read_line(&mut input_string).unwrap();
-
 			input_string.retain(|c| !c.is_whitespace());
 
 			return match input_string.as_str() {
@@ -77,9 +86,27 @@ impl Taskable for TestTaskA {
 				"e" => TaskControlFlow::Err(Rc::new(CustardNotInCycleError { offending_task: task_name.clone() })),
 				"f" => TaskControlFlow::FullReload,
 				"p" => TaskControlFlow::PartialReload({
-					let mut set = BTreeSet::new();
-					// set.insert(task_name.crate_name.clone());
-					Arc::new(set)
+					let mut definitely_reload = BTreeSet::new();
+
+					println!("Reload this crate? (y/n)");
+
+					let mut input_string = "".to_owned();
+
+					std::io::stdin().read_line(&mut input_string).unwrap();
+					input_string.retain(|c| !c.is_whitespace());
+					input_string = input_string.to_lowercase();
+
+					match input_string.as_str() {
+						"y" | "yes" | "1" => {
+							definitely_reload.insert(task_name.crate_name.clone());
+						}
+						"n" | "no" | "0" => {}
+						_ => {
+							println!("Didn't receive valid command (y/yes/1/n/no/0), continuing");
+							return TaskControlFlow::Continue;
+						}
+					}
+					Arc::new(definitely_reload)
 				}),
 				"a" => TaskControlFlow::StopAll,
 				"s" => TaskControlFlow::StopThis,
